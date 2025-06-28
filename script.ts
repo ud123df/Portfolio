1a05a479fcd705152276473289d1ac2d92f4c192
ud123df <147704288+ud123df@users.noreply.github.com>
2025-06-28 08:06:10 +0530

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,16 @@
+console.log("hello world");
+function greet(Name) {
+    return "hello, " +Name + "!";
+}
+let greeting = greet("Udit Sharma")
+console.log(greeting);
+
+
+
+
+const submitBtn = document.getElementById("submit");
+console.log(submitBtn);
+
+let userName: string = "Your Name";
+const websiteTitle: string = "Your Website Title";
+submitBtn?.addEventListener
